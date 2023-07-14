08b2f0177356de87e62ea46c51147df2c76d92da
Ben Lings <ben.lings@bp.com>
2023-07-14 11:43:38 +0100

    Mix path + bool based navigationDestination

--- TestNavDestination/ContentView.swift
+++ TestNavDestination/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  TestNavDestination
+//  Test Navigation Path + Destination
 //
 //  Created by Ben Lings on 14/07/2023.
 //
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  
+  @State var path = [Int]()
+  
+  var body: some View {
+    NavigationStack(path: $path) {
+      VStack {
+        NavigationLink("Nav link to 1", value: 1)
+      }
+      .padding()
+      .navigationDestination(for: Int.self) { num in
+        IntView(num: num)
+      }
     }
+  }
+}
+
+struct IntView: View {
+  var num: Int
+  
+  @State var isDestVisible: Bool = false
+  
+  var body: some View {
+    VStack {
+      Text("Number \(num)")
+      Button("Show 2") {
+        isDestVisible = true
+      }
+    }
+    .navigationDestination(isPresented: $isDestVisible) {
+      Text("Destination 2")
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
