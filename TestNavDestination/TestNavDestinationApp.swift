f48e9c166976c1124380c55d45720ebebfd325fb
Ben Lings <ben.lings@bp.com>
2023-07-14 11:35:15 +0100

    Initial Commit

---
+++ TestNavDestination/TestNavDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNavDestinationApp.swift
+//  TestNavDestination
+//
+//  Created by Ben Lings on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestNavDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
